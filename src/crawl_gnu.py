061f1a879ce1f6ef04051dc182ee0cac4469007c
niuxu18 <993273596@qq.com>
2018-03-03 22:01:17 +0800

    craw tar . wget. gun ...

--- src/crawl_gnu.py
+++ src/crawl_gnu.py
@@ -2,68 +2,137 @@
 import urllib2
 import os
 import re
+import csv
+import my_util
 
-def download_file(main_url, file_name):
+# some constants
+REPOS_NAME = 'tar'
+LOG_PATTERN = my_util.get_log_function_pattern(REPOS_NAME)
+MAIN_URL = 'http://git.savannah.gnu.org'
+
+def store_patch_file(patch, counter, record, writer):
     """
-    @ param main url and file name\n
-    @ return true if success\n
-    @ involve download tar.gz from given url\n
+    @ param patch and counter\n
+    @ return new counter\n
+    @ involve check patch for log modification and store patch file if success\n
     """
-    # # dir url
-    # dir_url = main_url + file_name
-    # dir_page = urllib2.urlopen(dir_url)
-    # dir_html = dir_page.read().split('\n')
-    # for line in dir_html:
-    #     is_target = re.search(r'(postgre[a-zA-Z0-9]*postgre[a-zA-Z0-9]*-[0-9\.]*\.tar\.gz)', line, re.I)
-    #     if is_target:
-    #         file_name = is_target.group(1)
+    for line in patch:
+        if line.startswith('-') or line.startswith('+'):
+            is_log = re.search(LOG_PATTERN, line, re.I)
+            if is_log:
+                file_name = 'data/crawl/' + REPOS_NAME + '/patch_' + str(counter) + '.diff'
+                my_util.store_file(file_name, patch)
+                writer.writerow(record.append(file_name))
+                return counter + 1
+    
+    return counter
 
-    # if 'v' in file_name:
-    #     return False
-    # file url
-    file_url = main_url + file_name
-    file_content = urllib2.urlopen(file_url)
-    # do not download file downloaded
-    store_file_name = 'second/download/wget/repos/' + file_name
-    if os.path.isfile(store_file_name):
-        print 'already downloaded %s' %store_file_name
-        return True
-    download_tar_file = open(store_file_name, 'wb')
-    block_size = 8192
-    while True:
-        cache = file_content.read(block_size)
-        if not cache:
-            break
-        download_tar_file.write(cache)
-    download_tar_file.close()
-
-    return True
-
-def analyze_html(url):
+def generate_record(url, commit_info, counter, writer):
     """
-    @ param url\n
+    @ param commit info\n
     @ return nothing\n
-    @ involve fetch html of given url and deal with each href to download tar.gz\n
+    @ involve parse commit info and generate commit record\n
     """
-    # fetch html
-    response = urllib2.urlopen(url)
+    # regex parser for store [url, date, title, changes, patch_file]
+    date_pattern = r'Date: \w*, (\d* \w* \d*) \d*:\d*:.*'
+    title_pattern = r'Subject: (.*)'
+    patch_pattern = r'\* .*'
+    title = None
+    date = None
+    patch = None
+    # retrieve date, title and patch
+    for line in commit_info:
+        # try to find date(just one time)
+        if date is None:
+            is_date = re.search(date_pattern, line, re.I)
+            if is_date:
+                date = is_date.group(1)
+        # subject follows date
+        else:   
+            # find title(just one time)
+            if title is None:
+                is_title = re.search(title_pattern, line, re.I)
+                if is_title:
+                    title = is_title.group(1)
+            else:
+                # find patch(just one time)
+                is_patch = re.search(patch_pattern, line, re.I)
+                if is_patch:
+                    patch = commit_info[commit_info.index(line):]
+                    break
+                else:
+                   title += '\n' + line 
+    # retrieve modify and diff
+    changes = 0
+    modify_pattern = r'\d* \w* changed, (\d*) \w*\(.\), (\d*) \w*\(.\)'
+    diff_pattern = r'diff --git .*'
+    for line in patch:
+        # find changes(just one time)
+        if changes == 0:
+            is_modify = re.search(modify_pattern, line, re.I)
+            if is_modify:
+                for i in range(1, is_modify.lastindex + 1):
+                    changes += int(is_modify.group(i))
+        else:
+            # find first diff file
+            is_diff = re.search(diff_pattern, line, re.I)
+            if is_diff:
+                counter = store_patch_file(patch[patch.index(line):], counter, [url, date, title, changes], writer)
+                break
+            
+def analyze_commit(commit_page, counter, writer):
+    """
+    @ param commit page\n
+    @ return nothing\n
+    @ involve analyze commit page and store commit record[call generate code]\n
+    """
+    url = MAIN_URL + commit_page
+    commit_page = commit_page.replace('commit', 'patch')
+    response = urllib2.urlopen(MAIN_URL + commit_page)
+    commit_info = response.read()
+    commit_info = commit_info.split("\n")
+    # regex parser for store [url, time, title, explain, patch_file]
+    generate_record(url, commit_info, counter, writer)
+
+def analyze_commit_list(commit_list_page, total_counter, counter, writer):
+    """
+    @ param commit list page\n
+    @ return nothing\n
+    @ involve fetch and analyze commit list[call analyze commit]\n
+    """
+    # fetch commit list
+    response = urllib2.urlopen(MAIN_URL + commit_list_page)
     html = response.read()
     html = html.split("\n")
-    target_href_pattern = r'(?:href|HREF)="(wget-[0-9\.]*\.tar\.gz)">'
-    # target_href_pattern = r'href="(postfix-\d*\.\d*\.\d*\.tar.gz)"'
-    count = 0
+    commit_page_pattern = r"(?:href|HREF)='(/cgit/" + REPOS_NAME + r"\.git/commit/\?id=\w*)'>"
+
     # check html content against git-2.*.tar.gz
+    commit_count = 0
+    # every commit list page has at most 50 commits
     for line in html:
-        is_target = re.search(target_href_pattern, line, re.I)
-        if is_target:
-            file_name = is_target.group(1)
-            print 'now downloading %s' %(file_name)
-            if download_file(url, file_name):
-                count += 1
-                print "have downloaded %d:%s" %(count, file_name)
+        # one page has at most 50 commits
+        if commit_count >= 50:
+            break
+        is_commit = re.search(commit_page_pattern, line, re.I)
+        if is_commit:
+            analyze_commit(is_commit.group(1), counter)
+            commit_count += 1
+            total_counter += 1
+            if total_counter % 10 == 0:
+                print 'now analyzing %d total commit, find %d log commit' %(total_counter, counter)
+    # fetch next button
+    next_list_page_pattern = r"(?:href|HREF)='(/cgit/" + REPOS_NAME + r"\.git/log/\?ofs=.*)'>"
+    for line in html:
+        is_next_list = re.search(next_list_page_pattern, line, re.I)
+        if is_next_list:
+            analyze_commit_list(is_next_list.group(1), total_counter, counter, writer)
 
 """
 main function
 """
 if __name__ == "__main__":
-    analyze_html("http://mirrors.ustc.edu.cn/gnu/wget/")
+    record_file = file('data/analyze/' + REPOS_NAME + '_log_commit.csv', 'wb')
+    writer = csv.writer(record_file)
+    writer.writerow(['url', 'date', 'title', 'changes', 'file_name'])
+    analyze_commit_list('/cgit/' + REPOS_NAME + '.git/log/?ofs=50', 0, 0, writer)
+    record_file.close()
---
+++ src/my_util.py
@@ -0,0 +1,49 @@
+import re
+
+def retrieve_log_function(file_name):
+    """
+    @ param file name\n
+    @ return log function list\n
+    @ involve retrieve log function name from logging statement csv\n
+    """
+    log_functions = []
+    log_statement = open(file_name, 'rb')
+    lines = log_statement.readlines()
+
+    for line in lines:
+        log_function = line[0:line.find("@")]
+        if not log_function in log_functions:
+            log_functions.append(log_function)
+
+    log_statement.close()
+    return log_functions
+
+def get_log_function_pattern(repos):
+    """
+    @ param repos name\n
+    @ return log pattern\n
+    @ involve get log functions and create log pattern based on log functions\n
+    """
+    log_function_file_name = 'data/analyze/' + repos + '_logging_statement.csv'
+    log_functions = retrieve_log_function(log_function_file_name)
+
+    # concate log functions to create log pattern
+    regrex_string = r'('
+    for log_function in log_functions:
+        regrex_string += log_function + r'|'
+    regrex_string = regrex_string[:-1]
+    regrex_string += r')(\s*\(.*|\s*$)'
+
+    return regrex_string
+
+
+def store_file(file_name, file_content):
+    """
+    @ param patch and counter\n
+    @ return new counter\n
+    @ involve write file content into specified file\n
+    """
+    write_file = open(file_name, 'wb')
+    write_file.write('\n'.join(file_content))
+    write_file.close()
+
